8ff4dbdc56739fa607b457cedc9d1426e77481ec
Tim Coomar <tim@prototypedesign.org.uk>
2022-07-21 17:33:12 +0300

    Removed tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
-module.exports = {
-  content: [
-    './resources/**/*.antlers.html',
-    './resources/**/*.blade.php',
-    './resources/**/*.vue',
-    './content/**/*.md'
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
-}
+// module.exports = {
+//   content: [
+//     './resources/**/*.antlers.html',
+//     './resources/**/*.blade.php',
+//     './resources/**/*.vue',
+//     './content/**/*.md'
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [
+//     require('@tailwindcss/typography'),
+//   ],
+// }
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+// const mix = require('laravel-mix');
 
 /*
  |--------------------------------------------------------------------------
@@ -11,17 +11,17 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/site.js', 'public/js')
+// mix.js('resources/js/site.js', 'public/js')
 
-mix.postCss('resources/css/tailwind.css', 'public/css', [
-    require('postcss-import'),
-    require('tailwindcss/nesting'),
-    require('tailwindcss'),
-])
+// mix.postCss('resources/css/tailwind.css', 'public/css', [
+//     require('postcss-import'),
+//     require('tailwindcss/nesting'),
+//     require('tailwindcss'),
+// ])
 
-if (mix.inProduction()) {
-   mix.version();
-}
+// if (mix.inProduction()) {
+//    mix.version();
+// }
 
 /*
  |--------------------------------------------------------------------------
