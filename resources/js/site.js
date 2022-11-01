16b93fcba7e1df0f42b1939a8aa5bc75214e1b8c
Tim Coomar <tim@prototypedesign.org.uk>
2022-11-01 12:57:58 +0200

    Accordion js

--- resources/js/site.js
+++ resources/js/site.js
@@ -1,3 +1,7 @@
+////////////////////
+/// MAIN TOP MENU //
+////////////////////
+
 // Remove styles associated with no javascript
 
 const jsCheck = document.querySelector(".no-js");
@@ -16,3 +20,26 @@ archiveItem.addEventListener("click", function (evt) {
     evt.preventDefault();
     archiveMenu.classList.toggle("active");
 });
+
+////////////////////
+///// ACCORDION ////
+////////////////////
+
+const accordionLink = document.querySelector(".accordion-link");
+// let accordionContent = accordionLink.nextElementSibling;
+
+const accordion = document.getElementsByClassName("accordion-link");
+
+// accordionLink.addEventListener("click", function (evt) {
+//     evt.preventDefault();
+//     this.classList.toggle("active");
+//     accordionContent.classList.toggle("active");
+// });
+
+for (i = 0; i < accordion.length; i++) {
+    accordion[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        let accordionContent = this.nextElementSibling;
+        accordionContent.classList.toggle("active");
+    });
+}
